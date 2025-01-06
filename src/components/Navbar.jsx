3f48eb17d20693796265c0a34fc1210eb38a3f25
coollucky61 <coollucky61@gmail.com>
2025-01-07 02:10:45 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+
+function Navbar () {
+    return (
+        <>
+            <header>
+                <nav>
+                    <img src='src\assets\react-logo.svg' alt="React Logo" />
+                    <span>ReactFacts</span>
+                </nav>
+            </header>
+        </>
+    )
+}
+
+export default Navbar;
